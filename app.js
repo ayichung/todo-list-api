5c44780fb9742ec8b82da7ba1361a3ce5212f50a
ayichung <121764547+ayichung@users.noreply.github.com>
2023-08-14 16:16:15 -0400

    Setup server in app

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const app = express();
+const port = 8080;
+
+// dirname setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// import local modules
+import router from './routes/index.js';
+
+// response chain
+app.use('/', router);
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(port, function () {
+    console.log(`app listening on port ${port}.`)
+});
